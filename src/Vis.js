68659d79feece0f53fb46d85ab887f0eed25fdd4
Thomas Brettell <thomas.a.brettell@gmail.com>
2022-01-12 10:26:59 +1100

    add legend and description

--- src/Vis.js
+++ src/Vis.js
@@ -4,6 +4,8 @@ import data from '../scripts/f1-data-transformed.json';
 console.log(data);
 import './styles.scss';
 
+const keys = ['20', '10', '00', '90', '80'];
+
 const Vis = () => {
   useEffect(() => {
     let mouseDown = false;
@@ -64,7 +66,11 @@ const Vis = () => {
         d3.select(this).raise();
 
         d3.selectAll('.node').style('filter', function (d2) {
-          if (this !== self && (!neighboring(d.id, d2.id) && !neighboring(d2.id, d.id))) {
+          if (
+            this !== self &&
+            !neighboring(d.id, d2.id) &&
+            !neighboring(d2.id, d.id)
+          ) {
             return 'opacity(0.3)';
           } else {
             return null;
@@ -91,6 +97,30 @@ const Vis = () => {
           link.style('stroke-width', 0.25);
         }
       });
+
+    const legend = svg.append('g').attr('id', 'legend');
+
+    legend
+      .selectAll('g')
+      .data(keys)
+      .enter()
+      .append('g')
+      .attr('transform', (d, i) => `translate(${40 * i}, 10)`)
+      .append('rect')
+      .attr('width', '20')
+      .attr('height', '20')
+      .attr('fill', function (d) {
+        return color(d[0]);
+      });
+
+    legend
+      .selectAll('g')
+      .append('text')
+      .text((d) => `${d}s`)
+      .attr('font-size', '12px')
+      .attr('y', '35');
+
+    legend.append('text').text('The last decade the driver participated in.');
 
     node
       .append('circle')
@@ -163,6 +193,26 @@ const Vis = () => {
         </p>
       </header> */}
       <svg width={window.innerWidth} height={window.innerHeight} />
+      <div id="information">
+        <h1>F1 Drivers Connection Map</h1>
+        <p>
+          <a
+            href="https://www.formula1.com/en/results.html"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Source: F1 Results
+          </a>
+        </p>
+        <p>
+          This Force-Directed Graph shows the connection between all F1 drivers
+          between 2021 - 1989 and all the drivers they have driven against.
+          <br />
+          <br />
+          Drivers are plotted in closer proximity the more races they have
+          driven together.
+        </p>
+      </div>
     </>
   );
 };
